3c05000952d31bc76330875f812f2e0538d84fa5
ryoma okuda <catontheskate4423@gmail.com>
2021-04-16 13:17:34 +0900

    dockerのコンフリクト解消

--- frontend/old_stuffs/src/apis/getPath.tsx
+++
@@ -1,6 +0,0 @@
-export const getVisitingHistory = (): any => {
-  chrome.history.onVisited.addListener((result: any) => {
-    if (result.url.indexOf("amazon") === -1) return result
-    return result
-  })
-}
