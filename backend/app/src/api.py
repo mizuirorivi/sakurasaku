05bc95ee9b81b34f8fb53387b3243e7354638224
mizuirorivi <32mizuiro16@gmail.com>
2021-04-17 20:40:37 +0900

    arm用環境の構築

--- backend/app/src/api.py
+++ backend/app/src/api.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, request, abort, jsonify
 
 import url2score 
+import arm_url2score
 import url2productid
 import json
+import config
 #/api以下をデフォルトルート
 api = Blueprint('api',__name__,url_prefix='/api')
 
@@ -23,7 +25,10 @@ def get_retscore():
             'mesage': 'プロダクトコードが見つかりません'
             }}),503
     #product_idでscoreの取得
-    score = url2score.url2score(product_id)
+    if config.Config.arch == "arm":
+        score = arm_url2score.url2score(product_id)
+    else :
+        score = url2score.url2score(product_id)
     return jsonify({"score":str(score)})
 
 @api.route('/url2score',methods=['POST'])
--- backend/app/src/run.py
+++ backend/app/src/run.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
-
+import argparse
+import config
 
 from api import api
 
@@ -25,5 +26,12 @@ def show_page():
     return render_template('index.html',msg='Hello,',addditonal_msg="from Python")
 
 if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description="armアーキテクチャの選択")
+    parser.add_argument('-arch','-a',default="x86-64",help="armの場合には-a arm or -arch armを引数にとる")
+    args = parser.parse_args()
+    print(args.arch)
+    config.Config.arch = args.arch
     print('server run!!')
     app.run(host="0.0.0.0", port=8080, debug=True)
+
